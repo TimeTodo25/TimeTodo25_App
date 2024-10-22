677c047d6d61860a108a6e7f112e79b307bf6433
joy293 <opendoor2026@gmail.com>
2024-10-22 12:13:34 +0900

    ✨ [Feat] 모바일, 태블릿 바텀네비 tabIndex 연동

---
+++ lib/bloc/bottom_navigation_state.dart
@@ -0,0 +1,21 @@
+
+// 1. Bloc State
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class BottomNaviState<int> {
+  final int tabIndex;
+
+  BottomNaviState(this.tabIndex);
+}
+
+
+// 2. Bloc Cubit
+class BottomNaviCubit extends Cubit<BottomNaviState> {
+  BottomNaviCubit() : super(BottomNaviState(0));
+
+  // 탭 index 변경
+  void changeTab(int newIndex) {
+    emit(BottomNaviState(newIndex)); // 상태 변경
+    // _playAnimation();
+  }
+}
