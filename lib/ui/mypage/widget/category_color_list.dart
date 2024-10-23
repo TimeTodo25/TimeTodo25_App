5b4a4a8b4b488536bdf2f64e4a913e605acb7350
joy293 <opendoor2026@gmail.com>
2024-10-23 13:37:22 +0900

    💄 [Update] category colorList

--- lib/ui/mypage/widget/category_color_list.dart
+++ lib/ui/mypage/widget/category_color_list.dart
@@ -21,45 +21,52 @@ class _CategoryColorListState extends State<CategoryColorList> {
     Color(0xFFFF7A5D),
     Color(0xFFFFA4C1),
     Color(0xFF89F258),
-    Color(0xFFFF9C39),
   ];
+
+  // 선택한 컬러 정보를 저장할 변수
+  int? selectedColorIndex;
 
   @override
   Widget build(BuildContext context) {
+    // 버튼 리스트
     return GridView.builder(
-      shrinkWrap: true,
+        shrinkWrap: true,
         gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          // 1개의 행에 보여줄 item 개수
+            // 1개의 행에 보여줄 item 개수
             crossAxisCount: 4,
-          // 가로 세로 비율
-          // childAspectRatio: 1,
-            childAspectRatio: 4,
+            // 가로 세로 비율
+            childAspectRatio: 3,
             // 수평 padding
             mainAxisSpacing: 10,
-          // 수직 padding
-          crossAxisSpacing: 10
+            // 수직 padding
+            crossAxisSpacing: 10
         ),
         itemCount: categoryColor.length,
         itemBuilder: (BuildContext context, int index) {
-          return categoryColorButton(categoryColor[index]);
+          return categoryColorButton(categoryColor[index], () {
+            selectedColorIndex = index;
+          });
         });
   }
 }
 
-Widget categoryColorButton(Color color) {
-  return Container(
-    height: 30,
-    decoration: BoxDecoration(
-        color: color,
-        borderRadius: BorderRadius.all(Radius.circular(10)),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.grey.withOpacity(0.5),
-            blurRadius: 5,
-            spreadRadius: 0,
-            offset: Offset(0, 1),
-          ),
-        ]
+// 버튼 1개
+Widget categoryColorButton(Color color, VoidCallback onTap) {
+  return GestureDetector(
+    child: Container(
+      height: 30,
+      decoration: BoxDecoration(
+          color: color,
+          borderRadius: BorderRadius.all(Radius.circular(10)),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.5),
+              blurRadius: 5,
+              spreadRadius: 0,
+              offset: Offset(0, 1),
+            ),
+          ]),
     ),
+    onTap: onTap,
   );
 }
