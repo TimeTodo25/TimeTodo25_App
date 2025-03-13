c7dd867f0a241de4e1ee82ffb8fb955034a2e810
joy293 <opendoor2026@gmail.com>
2025-03-13 18:54:34 +0900

    ✨ [Feat] 필요한 Bloc 만 관찰할 수 있도록 코드 추가

--- lib/bloc/timetodo_observer.dart
+++ lib/bloc/timetodo_observer.dart
@@ -1,27 +1,40 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:time_todo/bloc/timer/all_timer/all_timer_bloc.dart';
+
+const observedBlocs = { AllTimerBloc };  // 관찰할 블록들
 
 class TimetodoObserver extends BlocObserver {
+  bool _shouldObserve(BlocBase bloc) => observedBlocs.contains(bloc.runtimeType);
+
   @override
   void onEvent(Bloc bloc, Object? event) {
+    if (_shouldObserve(bloc)) {
+      print('\n onEvent: ${bloc.runtimeType}, Event: $event');
+    }
     super.onEvent(bloc, event);
-    print('onEvent: ${bloc.runtimeType}, Event: $event');
   }
 
   @override
   void onChange(BlocBase bloc, Change change) {
+    if (_shouldObserve(bloc)) {
+      print('\n onChange: ${bloc.runtimeType}, Change: $change');
+    }
     super.onChange(bloc, change);
-    print('onChange: ${bloc.runtimeType}, Change: $change');
   }
 
   @override
   void onTransition(Bloc bloc, Transition transition) {
+    if (_shouldObserve(bloc)) {
+      print('\n onTransition: ${bloc.runtimeType}, Transition: $transition');
+    }
     super.onTransition(bloc, transition);
-    print('onTransition: ${bloc.runtimeType}, Transition: $transition');
   }
 
   @override
   void onError(BlocBase bloc, Object error, StackTrace stackTrace) {
+    if (_shouldObserve(bloc)) {
+      print('\n onError: ${bloc.runtimeType}, Error: $error');
+    }
     super.onError(bloc, error, stackTrace);
-    print('onError: ${bloc.runtimeType}, Error: $error');
   }
 }
